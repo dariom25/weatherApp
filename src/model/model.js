cfc7aade137d327652e7ae3f53039c5ffed6b737
dariom25 <dario@maciey.de>
2023-11-30 21:02:15 +0100

    Create model class and add method to log weather data

--- src/model/model.js
+++ src/model/model.js
@@ -2,8 +2,14 @@
 export default class Model {
     constructor() {
         this.location = "Hannover"
-        this.fetchWeatherData(this.location);
+        this.key = "bd332034335c4701ac3183417232611" // key ist möglicherweise falsch
+        this.fetchWeatherData(this.key, this.location);
     }
 
+    static async fetchWeatherData(key, location) {
+        const weatherDataJSON = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`);
+
+        console.log(weatherDataJSON);
+    }
 
 }
