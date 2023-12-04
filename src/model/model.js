4742628893d8a93c9128a6191f21c9c2c2e625d8
dariom25 <dario@maciey.de>
2023-12-04 20:51:36 +0100

    Update function and structure of class

--- src/model/model.js
+++ src/model/model.js
@@ -1,17 +1,28 @@
+import ProcessedWeather from "./weatherOfDay";
 
 export default class Model {
-    constructor() {
-        this.location = "hannover"
-        this.key = "bd332034335c4701ac3183417232611" // key ist möglicherweise falsch
-        Model.fetchWeatherData(this.key, this.location);
-    }
+  constructor() {
+    this.location = "hannover";
+    this.key = "bd332034335c4701ac3183417232611";
+    this.weatherData = Model.fetchWeatherData(this.key, this.location);
+    this.processedWeatherData = [];
+    Model.processWeatherData(this.weatherData);
+  }
 
-    static fetchWeatherData = async (key, location) => {
-        const weatherDataJSON = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, {
-            mode: 'cors'
-        });
+  static fetchWeatherData = async (key, location) => {
+    // hier noch error handling mit try catch
+    const weatherData = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=3`,
+      { mode: "cors" },
+    );
 
-        return weatherDataJSON.json();
-    }
+    const weatherDataJSON = await weatherData.json();
+    return weatherDataJSON;
+  };
 
+  static processWeatherData = (data) => {
+    data.then((weather) => {
+      console.log(weather)
+    });
+  };
 }
