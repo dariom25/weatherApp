34609ed4dcab0d81aaa9b658c82bd5013d37e2f8
dariom25 <dario@maciey.de>
2023-12-08 15:56:19 +0100

    Add method to fetch weather data

--- src/model/model.js
+++ src/model/model.js
@@ -1,5 +1,20 @@
-export default class Model{
+export default class Model {
   constructor() {
     this.location = "hanover";
+    this.key = "bd332034335c4701ac3183417232611";
+    Model.fetchWeatherData(this.key, this.location);
   }
+
+  static fetchWeatherData = async (key, location) => {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=3`,
+      { mode: "cors" },
+    );
+    const weatherData = await response.json();
+    Model.processWeatherData(weatherData);
+  };
+
+  static processWeatherData = (weatherData) => {
+    console.log(weatherData.forecast);
+  };
 }
