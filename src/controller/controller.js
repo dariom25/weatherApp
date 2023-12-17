fbf955d5296c024ff02675216f174e777fbc0ef9
dariom25 <dario@maciey.de>
2023-12-17 16:43:39 +0100

    Update the display correctly

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,5 +2,14 @@ export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.weatherData = this.model.fetchWeatherData("hanover");
+    this.updateDisplay(this.weatherData, 0);
   }
+
+  updateDisplay = (weatherObject, measurement) => {
+    weatherObject.then((weatherData) => {
+      this.view.render(weatherData[0], measurement);
+      this.view.renderHourlyWeather(weatherData[0], measurement);
+    });
+  };
 }
