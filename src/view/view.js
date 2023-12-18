853e28090e2915976e26ede4c0a27645328a8fa0
dariom25 <dario@maciey.de>
2023-12-18 16:48:17 +0100

    Change which measurement to take

--- src/view/view.js
+++ src/view/view.js
@@ -1,9 +1,12 @@
 export default class View {
   constructor() {
+    this.day = 0;
+    this.measurement = "0";
     this.cacheElements();
   }
 
   cacheElements = () => {
+    this.upcomingDays = document.querySelector(".upcoming-days");
     this.location = document.querySelector(".location");
     this.date = document.querySelector(".date");
     this.avgTemp = document.querySelector(".avg-temp-deg");
@@ -16,26 +19,26 @@ export default class View {
     this.toggleBtn = document.querySelector(".toggle-temperature");
   };
 
-  render = (weatherObject, measurement) => {
+  render = (weatherObject, ) => {
     this.location.textContent = weatherObject.location;
     this.date.textContent = weatherObject.date;
     this.sunrise.textContent = `Sunrise: ${weatherObject.sunrise}`;
     this.sunset.textContent = `Sunset: ${weatherObject.sunset}`;
 
-    if (measurement === "0") {
+    if (this.measurement === "0") {
       this.avgTemp.textContent = `${weatherObject.avgtempC} °C`;
       this.maxTemp.textContent = `${weatherObject.maxtempC} °C`;
       this.minTemp.textContent = `${weatherObject.mintempC} °C`;
-    } else if (measurement === "1") {
+    } else if (this.measurement === "1") {
       this.avgTemp.textContent = `${weatherObject.avgtempF} °F`;
       this.maxTemp.textContent = `${weatherObject.maxtempF} °F`;
       this.minTemp.textContent = `${weatherObject.mintempF} °F`;
     }
   };
 
-  renderHourlyWeather = (weatherObject, measurement) => {
+  renderHourlyWeather = (weatherObject) => {
     weatherObject.hourlyWeather.forEach((hour) => {
-      if (measurement === "0") {
+      if (this.measurement === "0") {
         const weatherCardHTML = this.createWeatherCard(
           hour.hour,
           hour.tempC,
@@ -43,7 +46,7 @@ export default class View {
           "°C"
         );
         this.cards.insertAdjacentHTML("beforeend", weatherCardHTML);
-      } else if (measurement === "1") {
+      } else if (this.measurement === "1") {
         const weatherCardHTML = this.createWeatherCard(
           hour.hour,
           hour.tempF,
@@ -67,19 +70,20 @@ export default class View {
   };
 
   bindToggleMeasurement = (handler) => {
-    this.toggleBtn.addEventListener("click", (event) => {
+    this.toggleBtn.addEventListener("click", () => {
       this.deleteWeatherCards();
-      const id = event.target.id;
-      handler(id);
-      if (id === "1") {
+      handler(this.measurement);
+      if (this.measurement === "1") {
         this.toggleBtn.textContent = "Toggle Temperature to °C";
-        this.toggleBtn.id = 0;
-      } else if (id === "0") {
+        this.measurement = "0";
+      } else if (this.measurement === "0") {
         this.toggleBtn.textContent = "Toggle Temperature to °F";
-        this.toggleBtn.id = 1;
+        this.measurement = "1";
       }
     });
   };
+
+
 
   deleteWeatherCards = () => {
     const cardsParent = document.querySelector(".cards");
