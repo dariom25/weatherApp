94e131986112b82b71b7a2fe0ab8a8d324fc09e0
dariom25 <dario@maciey.de>
2023-12-20 14:39:40 +0100

    Render gif in updateDisplay

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,36 +3,40 @@ export default class Controller {
     this.model = model;
     this.view = view;
     this.weatherData = this.model.fetchWeatherData(this.model.location);
-    this.updateDisplay(this.weatherData);
+    this.gifUrl = this.model.fetchGif("cats");
+    this.updateDisplay(this.weatherData, this.gifUrl);
     this.bindEvents();
   }
 
-  updateDisplay = (weatherObject) => {
+  updateDisplay = (weatherObject, gifUrl) => {
     this.view.deleteWeatherCards();
     weatherObject.then((weatherData) => {
-      this.view.render(weatherData[this.view.day], this.view.measurement);
-      this.view.renderHourlyWeather(weatherData[this.view.day], this.view.measurement);
+      this.view.render(weatherData[this.view.day]);
+      this.view.renderHourlyWeather(weatherData[this.view.day]);
+    });
+    gifUrl.then((gif) => {
+      this.view.renderGif(gif);
     });
   };
 
   handleToggleMeasurement = (id) => {
     this.updateDisplay(this.weatherData, id);
-  }
+  };
 
   handleSwitchDay = () => {
-    this.updateDisplay(this.weatherData, this.view.measurement)
-  }
+    this.updateDisplay(this.weatherData, this.view.measurement);
+  };
 
   handleSearchLocation = () => {
     this.model.location = this.view.getUserInput();
     this.weatherData = this.model.fetchWeatherData(this.model.location);
     this.view.emptyInput();
     this.updateDisplay(this.weatherData);
-  }
+  };
 
   bindEvents = () => {
     this.view.bindToggleMeasurement(this.handleToggleMeasurement);
     this.view.bindSwitchDay(this.handleSwitchDay);
     this.view.bindSearchLocation(this.handleSearchLocation);
-  }
+  };
 }
